dea712e40088503482370e1ba75c50617702e35c
yannicklimmer <yannick.limmer@icloud,com>
2022-08-30 00:33:54 +0200

    Allow arbitrary activation functions.

--- pt/layers/DmlLinear.py
+++ pt/layers/DmlLinear.py
@@ -1,16 +1,26 @@
+from typing import Callable
+
 import torch
-from torch import nn
+from torch import nn, autograd
 
 
 class DmlLinear(nn.Module):
 
-    def __init__(self, input_dim: torch.Tensor, output_dim: torch.Tensor):
+    def __init__(
+            self,
+            input_dim: torch.Tensor,
+            output_dim: torch.Tensor,
+            activation: Callable[[torch.Tensor], torch.Tensor],
+    ):
         super().__init__()
         self.weight = nn.Parameter(torch.randn(input_dim, output_dim))
         self.bias = nn.Parameter(torch.randn(output_dim))
 
-        self.activation = nn.functional.softplus
-        self.activation_derivative = nn.functional.sigmoid
+        self.activation = activation
+
+    def activation_derivative(self, x: torch.Tensor) -> torch.Tensor:
+        # noinspection PyTypeChecker
+        return torch.diagonal(autograd.functional.jacobian(self.activation, x))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.activation((x @ self.weight) + self.bias)
@@ -20,4 +30,3 @@ class DmlLinear(nn.Module):
         For a given forward pass with input `x`, i.e. `y = forward(x)`; we can compute the greek of the output.
         """
         return (y @ self.weight.T) * self.activation_derivative(x)[:, None]
-

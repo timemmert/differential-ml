dc5e72bdd5fbd265b867000c45468af819a8cea4
yannicklimmer <yannick.limmer@icloud,com>
2022-09-04 11:35:36 +0200

    Provide a converter.
    Takes any arbitrary torch module and translates it to a DmlModule.

--- pt/modules/DmlModule.py
+++ pt/modules/DmlModule.py
@@ -9,3 +9,14 @@ class DmlModule(torch.nn.Module):
         y = self.forward(x)
         # noinspection PyTypeChecker
         return y, torch.autograd.functional.jacobian(self.forward, x)
+
+    @classmethod
+    def convert(cls, module: torch.nn.Module) -> None:
+        # TODO. Check that this is not a subclass of DmlModule already
+
+        class DmlVersionOfModule(cls, type(module)):
+            # TODO. Check if it is possible to get a more comprehensive name for this.
+            pass
+
+        module.__class__ = DmlVersionOfModule
+        

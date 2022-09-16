1f9009cbad84c69b71df86f4ec4a70199edcb1f2
Tim Emmert <emmertt@amazon.co.uk>
2022-09-16 17:01:28 +0100

    Adjust data generation to handle multiple inputs/outputs

--- examples/data_generation/functions.py
+++ examples/data_generation/functions.py
@@ -1,7 +1,9 @@
 from enum import Enum
 from typing import Callable, Tuple
 
+import numpy as np
 import torch
+from torch.autograd.functional import jacobian
 
 from examples.data_generation.random_data import uniform_random_in_range
 
@@ -12,14 +14,26 @@ class OutputFormat(Enum):
     NUMPY = 2
 
 
-def aad_datagen(x: torch.tensor, datagen_function: Callable[[torch.tensor], torch.tensor], output_format: OutputFormat = OutputFormat.NUMPY):
+def aad_datagen(
+        x: Tuple[torch.tensor, np.array],
+        datagen_function: Callable[[torch.tensor], torch.tensor],
+        output_format: OutputFormat = OutputFormat.NUMPY
+):
+    if not torch.is_tensor(x):
+        x = torch.tensor(x, requires_grad=True)
+    if not x.requires_grad:
+        x.requires_grad_(True)
+
     y = datagen_function(x)
+    """
+    The following line is not ideal, the reason is discussed here:
+    https://discuss.pytorch.org/t/jacobian-functional-api-batch-respecting-jacobian/84571
+    
+    As the input x is of shape (n, n_x) and y of shape (n, n_y), dydx is computed as (n, n_y, n, n_x). As n becomes
+    bigger, the computation becomes slower due to the differentiation in the unnecessary dimension.
+    """
+    dydx = jacobian(datagen_function, x).sum(dim=2)
 
-    x.retain_grad()
-    y.backward(torch.ones(x.shape[0]))
-    dydx = x.grad
-
-    y = y.reshape(-1, 1)
     if output_format == OutputFormat.NUMPY:
         return x.detach().numpy(), y.detach().numpy(), dydx.detach().numpy()
     elif output_format == OutputFormat.TORCH:
@@ -28,21 +42,44 @@ def aad_datagen(x: torch.tensor, datagen_function: Callable[[torch.tensor], torc
         raise NotImplementedError(f"Output format {output_format} not supported")
 
 
-def trigonometric_one_dim(n: int, x_range: Tuple[float, float] = (-10, 10), output_format: OutputFormat = OutputFormat.NUMPY):
+def trigonometric_one_in_one_out(n: int, x_range: Tuple[float, float] = (-10, 10),
+                                 output_format: OutputFormat = OutputFormat.NUMPY):
     x = uniform_random_in_range(n, 1, start=x_range[0], end=x_range[1])
-    return aad_datagen(x, lambda x_: torch.sin(x_[:, 0]) + x_[:, 0] * torch.cos(x_[:, 0]), output_format=output_format)
+    return aad_datagen(x, lambda x_: (torch.sin(x_[:, 0]) + x_[:, 0] * torch.cos(x_[:, 0]))[:, None], output_format=output_format)
 
 
-def quadratic_one_dim(a: float, n: int, x_range: Tuple[float, float] = (-10, 10), output_format: OutputFormat = OutputFormat.NUMPY):
+def quadratic_one_in_one_out(a: float, n: int, x_range: Tuple[float, float] = (-10, 10),
+                             output_format: OutputFormat = OutputFormat.NUMPY):
     x = uniform_random_in_range(n, 1, start=x_range[0], end=x_range[1])
-    return aad_datagen(x, lambda x_: a * x_[:, 0] ** 2, output_format=output_format)
+    return aad_datagen(x, lambda x_: (a * x_[:, 0] ** 2)[:, None], output_format=output_format)
 
 
-def quadratic_two_dim(a: float, n: int, x_range: Tuple[float, float] = (-10, 10), output_format: OutputFormat = OutputFormat.NUMPY):
+def quadratic_two_in_one_out(a: float, n: int, x_range: Tuple[float, float] = (-10, 10),
+                             output_format: OutputFormat = OutputFormat.NUMPY):
     x = uniform_random_in_range(n, 2, start=x_range[0], end=x_range[1])
-    return aad_datagen(x, lambda x_: a * x_[:, 0] ** 2 * x_[:, 1] ** 2, output_format=output_format)
+    return aad_datagen(x, lambda x_: (a * x_[:, 0] ** 2 * x_[:, 1] ** 2)[:, None], output_format=output_format)
 
 
-def polynomial_trigonometric_two_dim(n: int, x_range: Tuple[float, float] = (-10, 10), output_format: OutputFormat = OutputFormat.NUMPY):
+def polynomial_trigonometric_two_in_one_out(n: int, x_range: Tuple[float, float] = (-10, 10),
+                                            output_format: OutputFormat = OutputFormat.NUMPY):
     x = uniform_random_in_range(n, 2, start=x_range[0], end=x_range[1])
-    return aad_datagen(x, lambda x_: x_[:, 0] ** 3 * x_[:, 1] ** 2 + torch.sin(x_[:, 0]) * x_[:, 1], output_format=output_format)
+    return aad_datagen(x, lambda x_: (x_[:, 0] ** 3 * x_[:, 1] ** 2 + torch.sin(x_[:, 0]) * x_[:, 1])[:, None],
+                       output_format=output_format)
+
+
+def trigonometric_two_in_two_out(
+        n: int,
+        x_range: Tuple[float, float] = (-10, 10),
+        output_format: OutputFormat = OutputFormat.NUMPY
+):
+    x = uniform_random_in_range(n, 2, start=x_range[0], end=x_range[1])
+    return aad_datagen(
+        x,
+        lambda x_: torch.stack(
+            (
+                torch.sin(x_[:, 0]), torch.cos(x_[:, 1])
+            ),
+            dim=1
+        ),
+        output_format=output_format
+    )
--- examples/data_generation/random_data.py
+++ examples/data_generation/random_data.py
@@ -5,8 +5,7 @@ def uniform_random_in_range(
         *size: int,
         start: float = -10,
         end: float = 10,
-        requires_grad: bool = True
 ) -> torch.tensor:
     if end <= start:
         raise ValueError("Start value for random number generation needs to be lower than end value.")
-    return torch.rand(size, requires_grad=requires_grad) * torch.tensor((end - start)) + torch.tensor(start)
+    return torch.rand(size) * torch.tensor((end - start)) + torch.tensor(start)

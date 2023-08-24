efa5da1c9c537c3a52b2309e9a9fc5df4da7ca18
Tim Emmert <tim.emmert@tum.de>
2023-08-24 15:01:11 +0200

    Implement better Test cases for PyTorch

--- pt/examples/ApproximateFunctions.py
+++ pt/examples/ApproximateFunctions.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch import nn
 from torch.nn import MSELoss
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from pt.DmlTrainer import DmlTrainer, Trainer
+from pt.DmlTrainer import DmlTrainer
 from pt.modules.DmlDataset import DmlDataset
-from pt.modules.DmlFeedForward import DmlFeedForward, FeedForwardTestNetwork
+from pt.modules.DmlFeedForward import DmlFeedForward
 from pt.modules.DmlLoss import DmlLoss
 from util.data_util import DataNormalizer
 
@@ -26,7 +25,7 @@ def polynomial_and_trigonometric(n):
 
 
 if __name__ == "__main__":
-    n_train = 1000
+    n_train = 100
     n_test = 1000
     x_train, y_train, dydx_train = polynomial_and_trigonometric(n_train)
     x_test, y_test, dydx_test = polynomial_and_trigonometric(n_test)
@@ -51,45 +50,57 @@ if __name__ == "__main__":
 
     n_layers = 2
     hidden_layer_sizes = 100
-    lr = 0.1
+    lr_dml = 0.1
+    lr_std = 0.1
+    batch_size = 32
+    n_epochs = 100
+    activation = torch.nn.Softmax(-1)
+    # activation = torch.nn.ReLU()
 
     dml_net = DmlFeedForward(
         normalizer.input_dimension,
         normalizer.output_dimension,
         n_layers,
         hidden_layer_sizes,
-        torch.nn.Softmax(-1),
+        activation,
     )
-    net = FeedForwardTestNetwork()
 
     dml_loss = DmlLoss(
         _lambda=1,  # Weight of differentials in Loss
         _input_dim=normalizer.input_dimension,
         _lambda_j=normalizer.lambda_j,
     )
-    loss = MSELoss()
-
-    dml_sgd = torch.optim.Adam(lr=lr, params=dml_net.parameters())
+    dml_sgd = torch.optim.Adam(lr=lr_dml, params=dml_net.parameters())
     dml_trainer = DmlTrainer(dml_net, dml_loss, optimizer=dml_sgd)
 
-    sgd = torch.optim.Adam(lr=lr, params=net.parameters())
-    trainer = Trainer(net, loss, optimizer=sgd)
-
-
+    # Same as the DML net, but will be trained with lambda = 0
+    net = DmlFeedForward(
+        normalizer.input_dimension,
+        normalizer.output_dimension,
+        n_layers,
+        hidden_layer_sizes,
+        activation,
+    )
+    loss = DmlLoss(
+        _lambda=0,
+        _input_dim=normalizer.input_dimension,
+        _lambda_j=normalizer.lambda_j,
+    )
+    sgd = torch.optim.Adam(lr=lr_std, params=net.parameters())
+    trainer = DmlTrainer(net, loss, optimizer=sgd)
 
     dml_dataset = DmlDataset(x_train_normalized, y_train_normalized, dy_dx_train_normalized)
 
     train_size = int(0.8 * len(dml_dataset))
     valid_size = len(dml_dataset) - train_size
     train_set, validation_set = torch.utils.data.random_split(dml_dataset, [train_size, valid_size])
-    batch_size = 128
     shuffle = True  # Set to True if you want to shuffle the data
 
     # Create a DataLoader using the custom dataset
     dataloader_train = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
     dataloader_valid = DataLoader(validation_set, batch_size=batch_size, shuffle=shuffle)
 
-    for epoch in range(20):
+    for epoch in range(n_epochs):
         pbar_train = tqdm(dataloader_train)
         pbar_valid = tqdm(dataloader_valid, disable=True)
         # Train loop
@@ -105,15 +116,15 @@ if __name__ == "__main__":
             step = trainer.step(
                 inputs,
                 targets,
+                gradients,
             )
-        pbar_train.set_description(f"Loss DML: {step_dml.loss.item()}, Loss: {step.loss.item()}")
+            pbar_train.set_description(f"Loss DML: {step_dml.loss.item()}, Loss: {step.loss.item()}")
         with torch.no_grad():
             valid_error_dml = 0
             valid_error = 0
             for batch in pbar_valid:
                 inputs = batch['x']  # Access input features
                 targets = batch['y']  # Access target labels
-                gradients = batch['dydx']  # Access dy/dx values
 
                 outputs_dml = dml_net(inputs)
                 outputs = net(inputs)
@@ -122,20 +133,21 @@ if __name__ == "__main__":
                 valid_error += float(MSELoss()(outputs, targets))
             print(f"Validation Loss DML {valid_error_dml}\nValidation Loss {valid_error}")
 
-    print((list(dml_net.parameters())))
-
     dml_net.eval()
     y_out_test = [
         normalizer.unscale_y(dml_net(torch.as_tensor(x, dtype=torch.float32)).item()) for x in normalizer.scale_x(x_test)
     ]
-    net.eval()
-    """y_out_test = [
-        normalizer.unscale_y(net(torch.as_tensor(x, dtype=torch.float32)).item()) for x in
-        normalizer.scale_x(x_test)
-    ]"""
-
-    print('Error:', np.mean((np.array(y_out_test) - y_test) ** 2))
-
+    print('Test Error DML:', np.mean((np.array(y_out_test) - y_test) ** 2))
     plt.scatter(x_test, y_out_test, s=.1)
     plt.scatter(x_test, y_test, s=.1)
     plt.show()
+
+    net.eval()
+    y_out_test = [
+        normalizer.unscale_y(net(torch.as_tensor(x, dtype=torch.float32)).item()) for x in
+        normalizer.scale_x(x_test)
+    ]
+    print('Test Error Standard:', np.mean((np.array(y_out_test) - y_test) ** 2))
+
+
+
